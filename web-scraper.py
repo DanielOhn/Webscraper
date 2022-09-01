3af375f293e6fd28775b7d0392bacc43169df973
unknown <ohndaniel@gmail.com>
2022-08-31 23:26:55 -0700

    Add Requests and BeautifulSoup to project.  Update readPage function.

--- web-scraper.py
+++ web-scraper.py
@@ -1,6 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
 
 class Webscraper():
     def __init__(self):
         pass
 
-    
+    def readPage(self, url):        
+        req = requests.get(url)
+        parse = BeautifulSoup(req.content, "html.parser");
+
+        return parse
+
+
+webscraper = Webscraper()
+print(webscraper.readPage("https://anilist.co/character/2"))

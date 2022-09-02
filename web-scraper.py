75bfc21824cb4ee5ff8b6bd0ea9914506c177a57
unknown <ohndaniel@gmail.com>
2022-09-02 01:01:48 -0700

    Add extractData function.

--- web-scraper.py
+++ web-scraper.py
@@ -3,14 +3,36 @@ import requests
 
 class Webscraper():
     def __init__(self):
-        pass
+        self.data = {}
 
     def readPage(self, url):        
         req = requests.get(url)
-        parse = BeautifulSoup(req.content, "html.parser");
+        parse = BeautifulSoup(req.content, "html.parser")
 
         return parse
 
+    def extractData(self, url):
+        res = self.readPage(url)
+
+        character = {}
+
+        name = res.find('h1')
+        character['name'] = name.text
+
+        image = res.find('img')
+        character['image'] = image['src']
+
+        # Gets Age, Birthdate, Gender
+        data = res.find_all("div", {"class": "data-point"})
+        for d in data:
+            txt = d.text.rstrip().split(":")
+            character[txt[0]] = txt[1]
+
+
+        print(character)
+
+
+
 
 webscraper = Webscraper()
-print(webscraper.readPage("https://anilist.co/character/2"))
+webscraper.extractData("https://anilist.co/character/2")
